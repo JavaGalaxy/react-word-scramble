64c7178c2c41d944b346d78748db11a438916cc4
Amogh Shendre <amogh.shendre@gmail.com>
2025-04-01 20:24:47 -0500

    base redux setup

---
+++ src/appState.ts
@@ -0,0 +1,69 @@
+import { getRandomWord } from "./getRandomWord";
+
+export type Phase = "pre-game" | "in-game" | "post-game";
+
+export interface BaseState {
+  phase: Phase;
+}
+
+export interface PreGameState extends BaseState {
+  phase: "pre-game";
+}
+
+export interface InGameState extends BaseState {
+  phase: "in-game";
+  goal: string;
+  guess: string;
+}
+
+export interface PostGameState extends BaseState {
+  phase: "post-game";
+  goal: string;
+}
+
+export type State = PreGameState | InGameState | PostGameState;
+
+export const getInitialState = (): State => {
+    return { phase : "pre-game" } 
+}
+
+export type StartGameAction = {
+    type: "start-game";
+  }
+  
+export type UpdateGuessAction = {
+    type: "update-guess";
+    newGuess: string;
+  }
+  
+export type Action = StartGameAction | UpdateGuessAction;
+
+export const reducer = (state: State, action: Action): State => {
+    switch(action.type) {
+        case "start-game": {
+            if(state.phase === "in-game"){
+                return state
+            }
+
+            return {
+                phase: "in-game",
+                goal: getRandomWord(),
+                guess: ""
+            }
+        }
+
+        case "update-guess": {
+            if(state.phase !== 'in-game'){
+                return state;
+            }
+
+            if(action.newGuess === state.goal){
+                return { phase: "post-game", goal: state.goal};
+            }
+
+            return { ...state, guess: action.newGuess}
+        }
+    }
+
+    return state
+}
---
+++ src/getRandomWord.ts
@@ -0,0 +1,8 @@
+import { getRandomElement } from "./util/getRandomElement";
+
+
+const WORDS = ["hello", "world"] as const;
+
+export const getRandomWord = (): string => {
+    return getRandomElement(WORDS)
+}
