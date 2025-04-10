c231be92474ddcc82657ebd89f60d624774ec2fc
Amogh Shendre <amogh.shendre@gmail.com>
2025-04-10 10:53:31 -0500

    formatted the files

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,9 @@ function App() {
 
     const fetchBannedWords = async () => {
       try {
-        const response = await fetch("https://unpkg.com/naughty-words@1.2.0/en.json");
+        const response = await fetch(
+          "https://unpkg.com/naughty-words@1.2.0/en.json",
+        );
         const bannedWords = await response.json();
 
         dispatch({ type: "load-banned-words", bannedWords });
@@ -123,11 +125,7 @@ function App() {
     }
   }
 
-  return (
-    <div className="App">
-      {content}
-    </div>
-  );
+  return <div className="App">{content}</div>;
 }
 
 export default App;
