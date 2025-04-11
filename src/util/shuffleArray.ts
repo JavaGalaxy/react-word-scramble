007423df3c1fe66f8ed22dc6ef1caf28a1e62992
Amogh Shendre <amogh.shendre@gmail.com>
2025-04-10 21:17:28 -0500

    input parameter type updated to be immutable

--- src/util/shuffleArray.ts
+++ src/util/shuffleArray.ts
@@ -1,4 +1,4 @@
-export const shuffleArray = <T>(array: T[]): T[] => {
+export const shuffleArray = <T>(array: readonly T[]): T[] => {
   const shuffledArray = [...array];
   for (let i = shuffledArray.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
